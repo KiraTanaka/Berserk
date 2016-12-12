e0ac8450e939e79594b87a3788ed23c578a3bac4
Aleksandr Pinin <alex.pinin@gmail.com>
2016-12-12 22:10:31 +0500

    start

---
+++ Application/IConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    /// <summary>
+    /// Инкапсулирует API для управления игроком.
+    /// </summary>
+    public interface IConnection
+    {
+    }
+}
---
+++ Application/PlayerContoller.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    /// <summary>
+    /// Класс-посредник для связи игроков из Domain и API для их управления.
+    /// </summary>
+    public class PlayerContoller
+    {
+        private IPlayer _player;
+        private IConnection _connection;
+
+        public PlayerContoller(IPlayer player, IConnection connection)
+        {
+            _player = player;
+            _connection = connection;
+        }
+    }
+}
