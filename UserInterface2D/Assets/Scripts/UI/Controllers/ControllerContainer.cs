ec40e703b79e9bba366f59d56a0985d62daa7d0c
Aleksandr Pinin <alex.pinin@gmail.com>
2017-02-26 14:17:58 +0500

    Cache controller removed

--- UserInterface2D/Assets/Scripts/UI/Controllers/ControllerContainer.cs
+++ UserInterface2D/Assets/Scripts/UI/Controllers/ControllerContainer.cs
@@ -6,19 +6,13 @@ namespace Assets.Scripts.UI.Controllers
 {
     public static class ControllerContainer
     {
-        public static ClientController LocalPlayerController { get; }
+        public static ClientController LocalPlayerController
+            => GameClientControllers.FirstOrDefault(x => x.isLocalPlayer);
 
-        public static List<ClientController> GameClientControllers { get; }
-
-        static ControllerContainer()
-        {
-            GameClientControllers = GameObject
+        public static List<ClientController> GameClientControllers
+            => GameObject
                 .FindGameObjectsWithTag("Gamer")
                 .Select(x => x.GetComponent<ClientController>())
                 .ToList();
-
-            LocalPlayerController = GameClientControllers
-                .FirstOrDefault(x => x.isLocalPlayer);
-        }
     }
 }
